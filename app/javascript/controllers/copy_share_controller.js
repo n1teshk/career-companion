f405c76fb3e65017c395bc68136d77ac705d366a
Nitesh Khirwal <nitesh.khr@gmail.com>
2025-09-03 14:44:18 +0200

    application_pages

--- app/javascript/controllers/copy_share_controller.js
+++ app/javascript/controllers/copy_share_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
     const contentId = event.params.contentId
     const contentElement = document.getElementById(contentId)
     if (contentElement) {
-      navigator.clipboard.writeText(contentElement.innerText).then(() => {
+      navigator.clipboard.writeText(contentElement.value || contentElement.innerText).then(() => {
         this.showFeedback(event.target, "bi-check", 2000)
       })
     }
@@ -25,6 +25,44 @@ export default class extends Controller {
     navigator.clipboard.writeText(url).then(() => {
       this.showFeedback(event.target, "bi-check", 2000)
     })
+  }
+
+  openShareModal(event) {
+    const shareUrl = event.params.shareUrl
+    const videoUrlInput = document.getElementById('videoUrl')
+    
+    // Set the video URL in the modal
+    videoUrlInput.value = shareUrl
+    
+    // Update share links
+    this.updateShareLinks(shareUrl)
+  }
+
+  updateShareLinks(shareUrl) {
+    const encodedUrl = encodeURIComponent(shareUrl)
+    const shareText = encodeURIComponent('Check out my video pitch!')
+    
+    // Update each share link
+    const emailShare = document.getElementById('emailShare')
+    const whatsappShare = document.getElementById('whatsappShare')
+    const linkedinShare = document.getElementById('linkedinShare')
+    const twitterShare = document.getElementById('twitterShare')
+    
+    if (emailShare) {
+      emailShare.href = `mailto:?subject=My Video Pitch&body=${shareText}%0A${encodedUrl}`
+    }
+    
+    if (whatsappShare) {
+      whatsappShare.href = `https://wa.me/?text=${shareText}%0A${encodedUrl}`
+    }
+    
+    if (linkedinShare) {
+      linkedinShare.href = `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`
+    }
+    
+    if (twitterShare) {
+      twitterShare.href = `https://twitter.com/intent/tweet?text=${shareText}&url=${encodedUrl}`
+    }
   }
 
   showFeedback(button, iconClass, duration) {
